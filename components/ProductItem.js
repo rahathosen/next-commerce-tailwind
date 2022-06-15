48fdc6f90a70bb3038aed3e452e0df8755a27182
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-06-15 23:45:27 +0600

    update cart

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -19,16 +19,26 @@ export default function ProductItem({ product }) {
 				</Link>
 
 				<div className="flex flex-col items-center justify-center pt-2 pb-2 ">
-					<Link href={`/product/${product.slug}`}>
-						<a>
-							<h2 className="text-xl font-semibold">{product.name}</h2>
-						</a>
-					</Link>
-					<p className="mb-2">{product.brand}</p>
-					<div className="flex  gap-10 justify-around items-center pt-2 pb-3">
-						<p className="font-bold text-lg">${product.price}</p>
+					<div className="flex flex-row gap-10 items-center">
+						<p className="font-bold md:text-4xl text-2xl text-gray-500">${product.price}</p>
+						<div className="flex flex-col items-center">
+							<Link href={`/product/${product.slug}`}>
+								<a>
+									<h2 className="md:text-xl font-bold text-gray-700">{product.name}</h2>
+								</a>
+							</Link>
+							<p className="mb-2 text-gray-700">{product.brand}</p>
+						</div>
+					</div>
+					<div className="flex  md:gap-10 gap-5 justify-around items-center pt-2 pb-3 md:px-0 ">
 						<button
-							className="relative flex bg-gray-200 border border-transparent py-2 rounded-md  px-4 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-300"
+							className="relative flex bg-neutral-100 border border-transparent py-2 rounded-md  px-4 items-center justify-center text-sm font-medium text-gray-600 hover:bg-neutral-200"
+							type="button"
+						>
+							Details
+						</button>
+						<button
+							className="relative flex bg-neutral-100 border border-transparent py-2 rounded-md  px-4 items-center justify-center text-sm font-medium text-gray-600 hover:bg-neutral-300"
 							type="button"
 						>
 							Add to cart
