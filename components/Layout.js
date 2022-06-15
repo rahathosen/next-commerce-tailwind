e1f4382ca76c56d6a6d88062afaa2074d0fd5c97
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-06-15 23:24:53 +0600

    banner remove

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-// import { XIcon } from '@heroicons/react/outline';
 const navigation = [
 	{
 		name: 'Facebook',
@@ -57,36 +56,7 @@ export default function Layout({ title, children }) {
 	return (
 		<div>
 			{/* Banner Start */}
-			<div className="relative bg-indigo-500">
-				<div className="max-w-7xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
-					<div className="pr-16 sm:text-center sm:px-16">
-						<p className="font-medium text-white">
-							<span className="md:hidden">Are you excited to use this application!</span>
-							<span className="hidden md:inline">
-								Big news! Are you excited to use this application!. {/*We&apos;re = we're*/}
-							</span>
-							<span className="block sm:ml-2 sm:inline-block">
-								<a
-									href="https://github.com/rahathosen/next-commerce-tailwind"
-									className="text-white font-bold underline"
-								>
-									{' '}
-									Learn more <span aria-hidden="true">&rarr;</span>
-								</a>
-							</span>
-						</p>
-					</div>
-					<div className="absolute inset-y-0 right-0 pt-1 pr-1 flex items-start sm:pt-1 sm:pr-2 sm:items-start">
-						<button
-							type="button"
-							className="flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white"
-						>
-							<span className="sr-only">Dismiss</span>
-							{/* <XIcon className="h-6 w-6 text-white" aria-hidden="true" /> */}
-						</button>
-					</div>
-				</div>
-			</div>
+
 			{/* Banner End */}
 			<Head>
 				<title>{title ? title + ' | NextCommerce' : 'NextCommerce'}</title>
