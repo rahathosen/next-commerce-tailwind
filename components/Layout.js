7f6a5460eb2d53ba599c36bb5d2c421c65d6c436
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-06-15 12:30:50 +0600

    added layout

---
+++ components/Layout.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function Layout({ title, children }) {
+	return (
+		<div>
+			<Head>
+				<title>{title ? title + ' | NextCommerce' : 'NextCommerce'}</title>
+				<meta name="description" content="ECommerce Website" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="flex min-h-screen flex-col justify-between">
+				<header>
+					<nav className="flex h-12 items-center px-4 justify-between shadow-md">
+						<Link href="/">
+							<div className="text-lg font-bold">NextCommerce</div>
+						</Link>
+						<div>
+							<Link href="/cart">
+								<a className="p-2">Cart</a>
+							</Link>
+							<Link href="/login">
+								<a className="p-2">Login</a>
+							</Link>
+						</div>
+					</nav>
+				</header>
+				<main className="container m-auto mt-4 mx-4">{children}</main>
+				<footer className="flex h-10 justify-center items-center shadow-inner">
+					Copyright @ 2022 NextCommerce
+				</footer>
+			</div>
+		</div>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
-import Head from 'next/head';
+import Layout from '../components/Layout';
 
 export default function Home() {
-	return (
-		<div>
-			<Head>
-				<title>Create Next App</title>
-			</Head>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</div>
-	);
+	return <Layout title="Home Page"> Home Page</Layout>;
 }
