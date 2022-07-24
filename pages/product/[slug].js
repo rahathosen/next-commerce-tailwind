8458e16dc24c3ce558b90b641de67be05494cb20
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-07-24 18:12:01 +0600

    implement out of stock

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -11,6 +11,7 @@ export default function ProductScreen() {
 	const { query } = useRouter();
 	const { slug } = query;
 	const product = data.products.find((x) => x.slug === slug);
+
 	if (!product) {
 		return <Layout>Product not found</Layout>;
 	}
@@ -18,6 +19,12 @@ export default function ProductScreen() {
 	const addToCartHandler = () => {
 		const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
 		const quantity = existItem ? existItem.quantity + 1 : 1;
+
+		if (product.countInStock > quantity) {
+			alert('Sorry, product is out of stock');
+			return;
+		}
+
 		dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
 	};
 	return (
