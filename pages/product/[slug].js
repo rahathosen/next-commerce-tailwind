3a015c4f0e8f69b402b898d3721ae2938172bb64
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-07-25 12:11:36 +0600

    fix right condition

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -20,7 +20,7 @@ export default function ProductScreen() {
 		const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
 		const quantity = existItem ? existItem.quantity + 1 : 1;
 
-		if (product.countInStock > quantity) {
+		if (product.countInStock < quantity) {
 			alert('Sorry, product is out of stock');
 			return;
 		}
