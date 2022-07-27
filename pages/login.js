f6d76243b6da5f236293ddd06daa617fdf641843
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-07-27 09:09:59 +0600

    🐞 fix: added email validation

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 export default function LoginScreen() {
+	const { handleSubmit, register, formState: { errors } } = useForm();
+	const submitHandler = ({ email, password }) => {};
+
 	return (
 		<Layout title="login">
-			<form className="mx-auto max-w-screen-md">
+			<form className="mx-auto max-w-screen-md" onSubmit={handleSubmit(submitHandler)}>
 				<h1 className="mb-4 text-xl font-bold text-neutral-700">Login</h1>
 				<div className="mb-4 font-semibold">
 					<label htmlFor="Email">Email</label>
-					<input type="email" id="email" className="w-full" autoFocus />
+					<input
+						type="email"
+						{...register('email', {
+							required: 'enter the email address',
+							pattern: {
+								value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+$/i,
+								message: 'invalid email address'
+							}
+						})}
+						id="email"
+						className="w-full"
+						autoFocus
+					/>
+					{errors.email && <div className="text-red-500 font-normal">{errors.email.message}</div>}
 				</div>
 				<div className="mb-4 font-semibold">
 					<label htmlFor="password">Password</label>
