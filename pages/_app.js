9d97bb349398677221fa64e5b351da6c0902d827
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-08-11 17:29:58 +0600

    ✨ feat: added unauthorize access prevent

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,36 @@
-import '../styles/globals.css';
-import { StoreProvider } from '../utils/Store';
-import { SessionProvider } from 'next-auth/react';
+import "../styles/globals.css";
+import { StoreProvider } from "../utils/Store";
+import { SessionProvider, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-	return (
-		<SessionProvider session={session}>
-			<StoreProvider>
-				{' '}
-				<Component {...pageProps} />
-			</StoreProvider>
-		</SessionProvider>
-	);
+  return (
+    <SessionProvider session={session}>
+      <StoreProvider>
+        {Component.auth ? (
+          <Auth>
+            <Component {...pageProps} />
+          </Auth>
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </StoreProvider>
+    </SessionProvider>
+  );
 }
 
+function Auth({ children }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/unauthorized?message=login required");
+    },
+  });
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return children;
+}
 export default MyApp;
--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get("cart")
     ? JSON.parse(Cookies.get("cart"))
-    : { cartItems: [] },
+    : { cartItems: [], shippingAddress: {} },
 };
 
 function reducer(state, action) {
@@ -39,6 +39,17 @@ function reducer(state, action) {
           cartItems: [],
           shippingAddress: { location: {} },
           paymentMethod: " ",
+        },
+      };
+    case "SAVE_SHIPPING_ADDRESS":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            ...action.payload,
+          },
         },
       };
     default:
