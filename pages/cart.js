bd0bced1df637cef39fa10e1966c5281b4232329
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-08-17 02:12:33 +0600

    🦄 refactor: style and responsive product details page

--- pages/cart.js
+++ pages/cart.js
@@ -23,87 +23,89 @@ function CartScreen() {
   };
   return (
     <Layout title="Shopping Cart">
-      <h1 className="mb-4 text-2xl font-semibold text-teal-900">
-        Shopping Cart
-      </h1>
-      {cartItems.length === 0 ? (
-        <div>
-          Cart is empty. <Link href="/">Go shopping</Link>
-        </div>
-      ) : (
-        <div className="grid md:grid-cols-4 md:gap-5">
-          <div className="overflow-x-auto md:col-span-3">
-            <table className="min-w-full ">
-              <thead className="border-b">
-                <tr>
-                  <th className="p-5 text-left">Item</th>
-                  <th className="p-5 text-right">Quantity</th>
-                  <th className="p-5 text-right">Price</th>
-                  <th className="p-5">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cartItems.map((item) => (
-                  <tr key={item.slug} className="border-b">
-                    <td>
-                      <Link href={`/product/${item.slug}`}>
-                        <a className="flex items-center">
-                          <Image
-                            src={item.image}
-                            alt={item.name}
-                            width={50}
-                            height={50}
-                          />
-                          &nbsp;
-                          {item.name}
-                        </a>
-                      </Link>
-                    </td>
-                    <td className="p-5 text-right">
-                      <select
-                        value={item.quantity}
-                        onChange={(e) =>
-                          updateCartHandler(item, e.target.value)
-                        }
-                      >
-                        {[...Array(item.countInStock).keys()].map((x) => (
-                          <option key={x + 1} value={x + 1}>
-                            {x + 1}
-                          </option>
-                        ))}
-                      </select>
-                    </td>
-                    <td className="p-5 text-right">${item.price}</td>
-                    <td className="p-5 text-center">
-                      <button onClick={() => removeItemHandler(item)}>
-                        <XCircleIcon className="h-5 w-5 text-rose-700 hover:text-rose-600" />
-                      </button>
-                    </td>
+      <div className="mx-auto max-w-screen-md xs:mx-5 sm:mx-5">
+        <h1 className="mb-4 text-2xl font-semibold text-teal-900">
+          Shopping Cart
+        </h1>
+        {cartItems.length === 0 ? (
+          <div>
+            Cart is empty. <Link href="/">Go shopping</Link>
+          </div>
+        ) : (
+          <div className="grid md:grid-cols-4 md:gap-5">
+            <div className="overflow-x-auto md:col-span-3">
+              <table className="min-w-full ">
+                <thead className="border-b">
+                  <tr>
+                    <th className="p-5 text-left">Item</th>
+                    <th className="p-5 text-right">Quantity</th>
+                    <th className="p-5 text-right">Price</th>
+                    <th className="p-5">Action</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {cartItems.map((item) => (
+                    <tr key={item.slug} className="border-b">
+                      <td>
+                        <Link href={`/product/${item.slug}`}>
+                          <a className="flex items-center">
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              width={50}
+                              height={50}
+                            />
+                            &nbsp;
+                            {item.name}
+                          </a>
+                        </Link>
+                      </td>
+                      <td className="p-5 text-right">
+                        <select
+                          value={item.quantity}
+                          onChange={(e) =>
+                            updateCartHandler(item, e.target.value)
+                          }
+                        >
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </select>
+                      </td>
+                      <td className="p-5 text-right">${item.price}</td>
+                      <td className="p-5 text-center">
+                        <button onClick={() => removeItemHandler(item)}>
+                          <XCircleIcon className="h-5 w-5 text-rose-700 hover:text-rose-600" />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="card p-5">
+              <ul>
+                <li className="pb-3 text-xl font-bold">
+                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) : $
+                  {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+                </li>
+                <li>
+                  <button
+                    onClick={() => {
+                      router.push("login?redirect=/shipping");
+                    }}
+                    className="primary-button w-full font-semibold"
+                  >
+                    CheckOut
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="card p-5">
-            <ul>
-              <li className="pb-3 text-xl font-bold">
-                Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) : $
-                {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
-              </li>
-              <li>
-                <button
-                  onClick={() => {
-                    router.push("login?redirect=/shipping");
-                  }}
-                  className="primary-button w-full font-semibold"
-                >
-                  CheckOut
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </Layout>
   );
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -32,102 +32,104 @@ export default function ProductScreen() {
   };
   return (
     <Layout title={product.name}>
-      <div className="py-2">
-        <Link href="/">back to products</Link>
-      </div>
-      <div className="grid md:grid-cols-3 md:gap-[40px]">
-        {/* div 1 */}
-        <div className=" rounded-2xl shadow-xl md:col-span-2">
-          <Image
-            className="rounded-2xl "
-            src={product.image}
-            alt={product.name}
-            width={640}
-            height={640}
-            layout="responsive"
-          />
+      <div className="mx-auto xs:mx-5 sm:mx-5">
+        <div className="py-2">
+          <Link href="/">back to products</Link>
         </div>
-        {/* div 1 end*/}
-        <div>
+        <div className="grid md:grid-cols-3 md:gap-[40px]">
+          {/* div 1 */}
+          <div className=" rounded-2xl shadow-xl md:col-span-2">
+            <Image
+              className="rounded-2xl "
+              src={product.image}
+              alt={product.name}
+              width={640}
+              height={640}
+              layout="responsive"
+            />
+          </div>
+          {/* div 1 end*/}
           <div>
-            <ul>
-              <li>
-                <h1 className="pb-10 text-center text-5xl font-bold text-stone-700">
-                  {product.name}
-                </h1>
-              </li>
-              <li className="flex items-center justify-between px-5 font-semibold text-stone-700">
-                Category{" "}
-                <span className="info-badge text-2xl font-bold ">
-                  {product.category}
-                </span>
-              </li>
-              <li className="flex items-center justify-between px-5 font-semibold text-stone-700">
-                Brand{" "}
-                <span className="text-2xl font-bold text-stone-700">
-                  {product.brand}
-                </span>
-              </li>
-              <li>
-                <div className="px-5 ">
-                  <button className="mr-1">
-                    <svg
-                      className="h-auto w-5 fill-current text-teal-400"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 576 512"
-                    >
-                      <path d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z" />
-                    </svg>
-                  </button>
-                  <span className=" font-medium text-slate-400">
-                    {product.rating} •{" "}
-                    <a href="#" className=" font-normal underline">
-                      {product.numReviews} reviews
-                    </a>
+            <div>
+              <ul>
+                <li>
+                  <h1 className="pb-10 text-center text-5xl font-bold text-stone-700 xs:pt-5 xs:text-3xl">
+                    {product.name}
+                  </h1>
+                </li>
+                <li className="flex items-center justify-between px-5 font-semibold text-stone-700 xs:font-normal">
+                  Category{" "}
+                  <span className="info-badge text-2xl font-bold ">
+                    {product.category}
                   </span>
+                </li>
+                <li className="flex items-center justify-between px-5 font-semibold text-stone-700 xs:font-normal">
+                  Brand{" "}
+                  <span className="text-2xl font-bold text-stone-700 xs:text-xl">
+                    {product.brand}
+                  </span>
+                </li>
+                <li>
+                  <div className="px-5 ">
+                    <button className="mr-1">
+                      <svg
+                        className="h-auto w-5 fill-current text-teal-400  xs:w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 576 512"
+                      >
+                        <path d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z" />
+                      </svg>
+                    </button>
+                    <span className=" font-medium text-slate-400">
+                      {product.rating} •{" "}
+                      <a href="#" className=" font-normal underline">
+                        {product.numReviews} reviews
+                      </a>
+                    </span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            {/* div 2 end */}
+            {/* div 3 */}
+            <div>
+              <div className=" flex items-center justify-between px-5  font-semibold text-stone-700 xs:font-normal">
+                <div>Price</div>
+                <div className=" text-3xl font-bold text-stone-700 xs:text-xl ">
+                  ${product.price}
                 </div>
-              </li>
-            </ul>
-          </div>
-          {/* div 2 end */}
-          {/* div 3 */}
-          <div>
-            <div className=" flex items-center justify-between px-5  font-semibold text-stone-700">
-              <div>Price</div>
-              <div className="  text-3xl font-bold text-stone-700 ">
-                ${product.price}
               </div>
-            </div>
-            <div className="mb-2 flex justify-between  px-5 font-semibold text-stone-700">
-              <div>Status</div>
-              <div>
-                {product.countInStock > 0 ? (
-                  <span className="stock-success-button">in stock</span>
-                ) : (
-                  <span className="stock-danger-button">unavailable</span>
-                )}
+              <div className="mb-2 flex justify-between  px-5 font-semibold text-stone-700 xs:font-normal">
+                <div>Status</div>
+                <div>
+                  {product.countInStock > 0 ? (
+                    <span className="stock-success-button">in stock</span>
+                  ) : (
+                    <span className="stock-danger-button">unavailable</span>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className="pt-5">
-              <button
-                className="primary-button inline-flex w-8/12 translate-x-20 items-center justify-center gap-2"
-                onClick={addToCartHandler}
-              >
-                {" "}
-                <ArchiveIcon className="justify-content h-5 w-5" />
-                add to cart
-              </button>
-            </div>
+              <div className="pt-5 pb-5">
+                <button
+                  className="primary-button inline-flex w-8/12 translate-x-20 items-center justify-center gap-2 xs:w-5/12 xs:translate-x-28"
+                  onClick={addToCartHandler}
+                >
+                  {" "}
+                  <ArchiveIcon className="justify-content h-5 w-5" />
+                  add to cart
+                </button>
+              </div>
 
-            <div className="pt-5">
-              <p>Description: {product.description}</p>
+              <div className="mb-5 flex justify-between px-5  pt-5 font-semibold text-stone-700 xs:font-normal">
+                <p>Description: {product.description}</p>
+              </div>
             </div>
           </div>
+          {/* div 3 end */}
         </div>
-        {/* div 3 end */}
-      </div>
-      <div>
-        <p>Extra Information</p>
+        <div>
+          <p>Extra Information</p>
+        </div>
       </div>
     </Layout>
   );
