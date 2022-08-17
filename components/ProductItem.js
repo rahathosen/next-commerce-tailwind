b6c806cffe1db484df2042cffe0f1973fb526382
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-08-17 10:21:54 +0600

    🧪 test: performance

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -3,17 +3,18 @@ import Link from "next/link";
 import Image from "next/image";
 
 //  For Blur placeholder down V
-const keyStr =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-const triplet = (e1, e2, e3) =>
-  keyStr.charAt(e1 >> 2) +
-  keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
-  keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-  keyStr.charAt(e3 & 63);
-const rgbDataURL = (r, g, b) =>
-  `data:image/gif;base64,R0lGODlhAQABAPAA${
-    triplet(0, r, g) + triplet(b, 255, 255)
-  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+// const keyStr =
+//   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+// const triplet = (e1, e2, e3) =>
+//   keyStr.charAt(e1 >> 2) +
+//   keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+//   keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+//   keyStr.charAt(e3 & 63);
+// const rgbDataURL = (r, g, b) =>
+//   `data:image/gif;base64,R0lGODlhAQABAPAA${
+//     triplet(0, r, g) + triplet(b, 255, 255)
+//   }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+
 // For Blur placeholder up  /\
 
 export default function ProductItem({ product }) {
