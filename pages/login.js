03d2f634f6912289a2c44d2273584639f22dce1d
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-08-19 21:04:35 +0600

    🦄 refactor:

--- pages/login.js
+++ pages/login.js
@@ -231,64 +231,6 @@ export default function LoginScreen() {
           </div>
         </div>
       </form>
-
-      {/* <form
-        className="mx-auto max-w-screen-md"
-        onSubmit={handleSubmit(submitHandler)}
-      >
-        <h1 className="mb-4 text-xl font-bold text-neutral-700">Login</h1>
-        <div className="mb-4 font-semibold">
-          <label htmlFor="Email">Email</label>
-          <input
-            type="email"
-            {...register("email", {
-              required: "enter the email address",
-              pattern: {
-                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+$/i,
-                message: "invalid email address",
-              },
-            })}
-            id="email"
-            className="w-full"
-            autoFocus
-          />
-          {errors.email && (
-            <div className="font-normal text-red-500">
-              {errors.email.message}
-            </div>
-          )}
-        </div>
-        <div className="mb-4 font-semibold">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            {...register("password", {
-              required: "enter the password",
-              minLength: {
-                value: 6,
-                message: "password is more than 5 character",
-              },
-            })}
-            id="password"
-            className="w-full"
-            autoFocus
-          />
-          {errors.password && (
-            <div className="text-red-500">{errors.password.message}</div>
-          )}
-        </div>
-        <div className="mb-4 font-semibold">
-          <button className="primary-button">Login</button>
-        </div>
-        <div className="mb-4 font-semibold">
-          Don&apos;t have an account ? &nbsp;
-          <Link href="register">
-            <span className="cursor-pointer text-blue-600 hover:text-blue-800">
-              Register
-            </span>
-          </Link>
-        </div>
-      </form> */}
     </Layout>
   );
 }
