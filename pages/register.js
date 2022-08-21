bc251eb3db31e7ebf901d7d4fc3f77debe22c59f
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-08-21 12:26:08 +0600

    fix: register ui update

--- pages/register.js
+++ pages/register.js
@@ -4,7 +4,8 @@ import { signIn, useSession } from "next-auth/react";
 import { useForm } from "react-hook-form";
 import Layout from "../components/Layout";
 import { getError } from "../utils/error";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
+import toast, { Toaster } from "react-hot-toast";
 import { useRouter } from "next/router";
 import axios from "axios";
 
@@ -48,93 +49,178 @@ export default function LoginScreen() {
   };
   return (
     <Layout title="Create Account">
+      <Toaster />
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-700">
+          Register a new Account
+        </h2>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          Or alreay an account,{" "}
+          <Link href={`/login?redirect=${redirect || "/"}`}>
+            <a className="font-medium text-sky-600 hover:text-sky-500">
+              log in
+            </a>
+          </Link>
+        </p>
+      </div>
       <form
-        className="mx-auto max-w-screen-md"
         onSubmit={handleSubmit(submitHandler)}
+        className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
       >
-        <h1 className="mb-4 text-xl">Create Account</h1>
-        <div className="mb-4">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            className="w-full"
-            id="name"
-            autoFocus
-            {...register("name", {
-              required: "Please enter name",
-            })}
-          />
-          {errors.name && (
-            <div className="text-red-500">{errors.name.message}</div>
-          )}
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            {...register("email", {
-              required: "Please enter email",
-              pattern: {
-                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                message: "Please enter valid email",
-              },
-            })}
-            className="w-full"
-            id="email"
-          ></input>
-          {errors.email && (
-            <div className="text-red-500">{errors.email.message}</div>
-          )}
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            {...register("password", {
-              required: "Please enter password",
-              minLength: { value: 6, message: "password is more than 5 chars" },
-            })}
-            className="w-full"
-            id="password"
-            autoFocus
-          ></input>
-          {errors.password && (
-            <div className="text-red-500 ">{errors.password.message}</div>
-          )}
-        </div>
-        <div className="mb-4">
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            className="w-full"
-            type="password"
-            id="confirmPassword"
-            {...register("confirmPassword", {
-              required: "Please enter confirm password",
-              validate: (value) => value === getValues("password"),
-              minLength: {
-                value: 6,
-                message: "confirm password is more than 5 chars",
-              },
-            })}
-          />
-          {errors.confirmPassword && (
-            <div className="text-red-500 ">
-              {errors.confirmPassword.message}
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <div className="space-y-6">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Full Name
+              </label>
+              <div className="mt-1">
+                <input
+                  {...register("email", {
+                    required: "enter the email address",
+                    pattern: {
+                      value:
+                        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+$/i,
+                      message: "invalid email address",
+                    },
+                  })}
+                  autoFocus
+                  {...register("name", {
+                    required: "Please enter name",
+                  })}
+                  className="peer block w-full rounded-md border border-slate-300 bg-white px-3  py-2 placeholder-slate-400 shadow-sm invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm"
+                />
+                {/* <p className="invisible text-sm text-pink-600 peer-invalid:visible">
+                  Please provide a valid email address.
+                </p> */}
+                {errors.name && (
+                  <div className="text-red-500">{errors.name.message}</div>
+                )}
+              </div>
             </div>
-          )}
-          {errors.confirmPassword &&
-            errors.confirmPassword.type === "validate" && (
-              <div className="text-red-500 ">Password do not match</div>
-            )}
-        </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  {...register("email", {
+                    required: "Please enter email",
+                    pattern: {
+                      value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                      message: "Please enter valid email",
+                    },
+                  })}
+                  type="email"
+                  id="email"
+                  className="peer block w-full rounded-md border border-slate-300 bg-white px-3  py-2 placeholder-slate-400 shadow-sm invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm"
+                />
+                {/* <p className="invisible text-sm text-pink-600 peer-invalid:visible">
+                  Please provide a valid email address.
+                </p> */}
+                {errors.email && (
+                  <div className="text-red-500">{errors.email.message}</div>
+                )}
+              </div>
+            </div>
 
-        <div className="mb-4 ">
-          <button className="primary-button">Register</button>
-        </div>
-        <div className="mb-4 ">
-          Already have an account? &nbsp;
-          <Link href={`/login?redirect=${redirect || "/"}`}>login</Link>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  type="password"
+                  {...register("password", {
+                    required: "Please enter password",
+                    minLength: {
+                      value: 6,
+                      message: "password is more than 5 chars",
+                    },
+                  })}
+                  id="password"
+                  autoFocus
+                  className="peer block w-full rounded-md border border-slate-300 bg-white px-3  py-2 placeholder-slate-400 shadow-sm invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm"
+                />
+                {errors.password && (
+                  <div className="text-red-500 ">{errors.password.message}</div>
+                )}
+              </div>
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Confirm Password
+              </label>
+              <div className="mt-1">
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  {...register("confirmPassword", {
+                    required: "Please enter confirm password",
+                    validate: (value) => value === getValues("password"),
+                    minLength: {
+                      value: 6,
+                      message: "confirm password is more than 5 chars",
+                    },
+                  })}
+                  autoFocus
+                  className="peer block w-full rounded-md border border-slate-300 bg-white px-3  py-2 placeholder-slate-400 shadow-sm invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none sm:text-sm"
+                />
+                {errors.confirmPassword && (
+                  <div className="text-red-500 ">
+                    {errors.confirmPassword.message}
+                  </div>
+                )}
+                {errors.confirmPassword &&
+                  errors.confirmPassword.type === "validate" && (
+                    <div className="text-red-500 ">Password do not match</div>
+                  )}
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-sky-600 focus:ring-sky-500"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-sky-600 hover:text-sky-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button className="flex w-full justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-0">
+                Register
+              </button>
+            </div>
+          </div>
         </div>
       </form>
     </Layout>
