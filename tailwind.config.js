b8708f2a6d5d4a795e656bf76ce02a528c2d33d9
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-08-25 01:01:32 +0600

    add img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  images: {
+    domains: ["source.unsplash.com"],
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
