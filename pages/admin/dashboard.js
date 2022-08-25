86b2b55bf59b879fa9b68aa42bb5b7e8e881b0b1
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-08-25 21:26:28 +0600

    profile ui update

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import Link from "next/link";
+// import { Bar } from "react-chartjs-2";
+
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from "chart.js";
+import React, { useEffect, useReducer } from "react";
+import Layout from "../../components/Layout";
+import { getError } from "../../utils/error";
+
+function AdminDashboardScreen() {
+  return (
+    <>
+      <Layout title="Admin Dashboard">
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div>
+            <ul>
+              <li>
+                <Link href="/admin/dashboard">
+                  <a className="font-boald">Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/admin/orders">Orders</Link>
+              </li>
+              <li>
+                <Link href="/admin/products">Products</Link>
+              </li>
+              <li>
+                <Link href="/admin/users">Users</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="md:col-span-3">
+            <h1 className="mb-4 text-xl">Admin Dashboard</h1>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
+
+AdminDashboardScreen.auth = { aminOnly: true };
+export default AdminDashboardScreen;
