b797c68a31bbd24b6689470985819bd9f547dc98
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-08-27 20:58:44 +0600

    fix:

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useReducer } from "react";
 import Layout from "../../components/Layout";
