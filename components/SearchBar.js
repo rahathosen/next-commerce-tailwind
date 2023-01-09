2e33c0c1edab08ff9e97c65ada80437efee9f4c3
rahathosen <70264343+rahathosen@users.noreply.github.com>
2023-01-10 02:59:43 +0600

    search bar change position

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -25,7 +25,7 @@ export default function SearchBar() {
             <div className="relative">
               <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                 <SearchIcon
-                  className="h-5 w-5 text-gray-400"
+                  className="h-5 w-5 text-neutral-400"
                   aria-hidden="true"
                 />
               </div>
@@ -33,7 +33,7 @@ export default function SearchBar() {
                 onChange={(e) => setQuery(e.target.value)}
                 id="search"
                 name="search"
-                className="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:border-neutral-500 focus:text-gray-900 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-neutral-500 sm:text-sm"
+                className="block w-full rounded-md border border-transparent bg-transparent py-2 pl-10 pr-3 text-sm placeholder-neutral-500 focus:border-neutral-500 focus:text-neutral-900 focus:placeholder-neutral-400 focus:outline-none focus:ring-1 focus:ring-neutral-500 sm:text-sm"
                 placeholder="Search"
                 type="search"
               />
