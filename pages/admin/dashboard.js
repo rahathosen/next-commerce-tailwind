bd124cc729775f2d0b422a86a62d005295e78192
rahathosen <70264343+rahathosen@users.noreply.github.com>
2023-01-10 01:45:10 +0600

    update dashboard ui

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import Link from "next/link";
 import { Bar } from "react-chartjs-2";
-
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -14,6 +13,38 @@ import {
 import React, { useEffect, useReducer } from "react";
 import Layout from "../../components/Layout";
 import { getError } from "../../utils/error";
+import {
+  CreditCardIcon,
+  UserCircleIcon,
+  UserGroupIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+
+const navigation = [
+  {
+    name: "Dashboard",
+    href: "/admin/dashboard",
+    icon: UserCircleIcon,
+    current: true,
+  },
+  {
+    name: "Orders",
+    href: "/admin/orders",
+    icon: PaperAirplaneIcon,
+    current: false,
+  },
+  {
+    name: "Products",
+    href: "/admin/products",
+    icon: CreditCardIcon,
+    current: false,
+  },
+  { name: "Users", href: "/admin/users", icon: UserGroupIcon, current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 ChartJS.register(
   CategoryScale,
@@ -78,35 +109,47 @@ function AdminDashboardScreen() {
   };
   return (
     <Layout title="Admin Dashboard">
-      <div className="grid  md:grid-cols-4 md:gap-5">
-        <div>
-          <ul>
-            <li>
-              <Link href="/admin/dashboard">
-                <a className="font-bold">
-                  <span className="text-lg">Dashboard</span>
+      <div className="lg:grid lg:grid-cols-12 lg:gap-x-5">
+        {/* <div className="grid md:grid-cols-4 md:gap-5"> */}
+        <aside className="py-6 px-2 sm:px-6 lg:col-span-3 lg:py-0 lg:px-0">
+          <nav className="space-y-1">
+            {navigation.map((item) => (
+              <Link key={item.name} href={item.href}>
+                <a
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-50 text-cyan-700 hover:bg-white hover:text-cyan-700"
+                      : "text-gray-900 hover:bg-gray-50 hover:text-gray-900",
+                    "group flex items-center rounded-md px-3 py-2 text-sm font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  <item.icon
+                    className={classNames(
+                      item.current
+                        ? "text-cyan-500 group-hover:text-cyan-500"
+                        : "text-gray-400 group-hover:text-gray-500",
+                      "-ml-1 mr-3 h-6 w-6 flex-shrink-0"
+                    )}
+                    aria-hidden="true"
+                  />
+                  <span className="truncate">{item.name}</span>
                 </a>
               </Link>
-            </li>
-            <li>
-              <Link href="/admin/orders">Orders</Link>
-            </li>
-            <li>
-              <Link href="/admin/products">Products</Link>
-            </li>
-            <li>
-              <Link href="/admin/users">Users</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="md:col-span-3">
-          <h1 className="mb-4 text-xl">Admin Dashboard</h1>
+            ))}
+          </nav>
+        </aside>
+
+        <div className="space-y-6 sm:px-6 lg:col-span-9 lg:px-0">
           {loading ? (
             <div>Loading...</div>
           ) : error ? (
-            <div className="alert-error">{error}</div>
+            <div className="alert-error px-6 sm:px-6 lg:px-0">{error}</div>
           ) : (
             <div>
+              <h3 className="px-6  text-lg font-medium leading-6 text-gray-900 sm:px-6 lg:px-0">
+                Admin Dashboard
+              </h3>
               <div className="grid grid-cols-1 md:grid-cols-4">
                 <div className="card m-5 p-5">
                   <p className="text-3xl">${summary.ordersPrice} </p>
