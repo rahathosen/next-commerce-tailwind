6a1bc01edec84d9ebeedc8d5974e356379d6e628
rahathosen <70264343+rahathosen@users.noreply.github.com>
2023-01-18 17:40:51 +0600

    fix paw

--- next.config.js
+++ next.config.js
@@ -36,7 +36,7 @@ const nextConfig = {
 
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  // disable: process.env.NODE_ENV === "development",
   register: true,
 });
 
