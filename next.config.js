037484b2582cc2f0a1f0b68513210ac319b18564
rahathosen <70264343+rahathosen@users.noreply.github.com>
2023-01-18 17:25:21 +0600

    test pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-});
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+// });
 
-const nextConfig = withPWA({
+// const nextConfig = withPWA({
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["res.cloudinary.com"],
+//   },
+//   i18n: {
+//     locales: ["en"],
+//     defaultLocale: "en",
+//   },
+// });
+
+// module.exports = nextConfig;
+
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
@@ -15,6 +28,16 @@ const nextConfig = withPWA({
     locales: ["en"],
     defaultLocale: "en",
   },
+  swcMinify: true,
+  compiler: {
+    // removeConsole: process.env.NODE_ENV !== "development",
+  },
+};
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
 });
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
