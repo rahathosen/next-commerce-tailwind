d0d2e648aa98808b8f08ee0f7f4b2b215f8a05d9
rahathosen <rahathosen.me@gmail.com>
2023-02-28 21:12:41 +0600

    Product size update

--- pages/admin/product/[id].js
+++ pages/admin/product/[id].js
@@ -399,6 +399,27 @@ export default function AdminProductEditScreen() {
                         htmlFor="email-address"
                         className="block text-sm font-medium text-gray-700"
                       >
+                        Size
+                      </label>
+                      <input
+                        className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-cyan-500 focus:outline-none focus:ring-cyan-500 sm:text-sm"
+                        type="text"
+                        id="size"
+                        {...register("size", {
+                          required: "Please enter size",
+                        })}
+                      />
+                      {errors.size && (
+                        <div className="text-red-500">
+                          {errors.size.message}
+                        </div>
+                      )}
+                    </div>
+                    <div className="col-span-6 sm:col-span-4">
+                      <label
+                        htmlFor="email-address"
+                        className="block text-sm font-medium text-gray-700"
+                      >
                         Count In Stock
                       </label>
                       <input
